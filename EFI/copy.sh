45ccca56285a89cbecdfc1fcc83540b78ff0baf9
Hykilpikonna <me@hydev.org>
2021-08-20 07:28:02 -0400

    [F] Fix copy script

--- EFI/copy.sh
+++ EFI/copy.sh
@@ -2,7 +2,8 @@
 
 # Mount EFI
 [[ -d /Volumes/EFI ]] || sudo diskutil mount /dev/disk3s1
+[[ -d /Volumes/EFI/EFI ]] || mkdir /Volumes/EFI/EFI
 
-rm -rf /Volumes/EFI/*
-cp -r ./OC /Volumes/EFI
-cp -r ./BOOT /Volumes/EFI
+rm -rf /Volumes/EFI/EFI/*
+cp -r ./OC /Volumes/EFI/EFI
+cp -r ./BOOT /Volumes/EFI/EFI
